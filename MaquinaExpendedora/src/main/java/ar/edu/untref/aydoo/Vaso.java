26a2d026dfed9e736ce7c47929debb1aa1e66f9a
Maxi Biscardi <maxibiscar@gmail.com>
2018-03-31 17:59:21 -0300

    compila(todas las clases y metodos creados)

---
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -0,0 +1,21 @@
+package ar.edu.untref.aydoo;
+
+public class Vaso {
+    int cantidadDeAzucar;
+
+    public int getCantidadDeAzucar() {
+        return cantidadDeAzucar;
+    }
+
+    public boolean tieneCafe() {
+        return true;
+    }
+
+    public boolean tieneAzucar() {
+        return true;
+    }
+
+    public boolean tieneTe() {
+        return true;
+    }
+}
