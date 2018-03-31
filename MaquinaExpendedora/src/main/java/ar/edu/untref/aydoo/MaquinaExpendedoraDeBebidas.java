df13be41282f04fe5b1512475d8682532d1ea32d
Maxi Biscardi <maxibiscar@gmail.com>
2018-03-31 18:23:23 -0300

    Todos los tests ok(con cosas internas a mejorar)

--- MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
@@ -2,8 +2,12 @@ package ar.edu.untref.aydoo;
 
 public class MaquinaExpendedoraDeBebidas {
     public void hacerCafeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+        unVaso.setContenido("cafe");
+        unVaso.setAzucar(cantidadAzucar);
     }
 
     public void hacerTeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+        unVaso.setContenido("te");
+        unVaso.setAzucar(cantidadAzucar);
     }
 }
--- MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -1,21 +1,50 @@
 package ar.edu.untref.aydoo;
 
+/*public enum contenido{
+    cafe;
+    azucar;
+    te;
+
+}*/
+
 public class Vaso {
     int cantidadDeAzucar;
+    boolean azucar = false;
+    String contenido = "";
 
     public int getCantidadDeAzucar() {
         return cantidadDeAzucar;
     }
 
     public boolean tieneCafe() {
-        return true;
+        if (contenido=="cafe"){
+            return true;
+        }else{
+            return false;
+        }
+
     }
 
     public boolean tieneAzucar() {
-        return true;
+        return azucar;
     }
 
     public boolean tieneTe() {
-        return true;
+        if (contenido == "te")
+        {   return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public void setAzucar(int azucarAgregada) {
+        if(azucarAgregada>0) {
+            this.cantidadDeAzucar = azucarAgregada;
+            this.azucar = true;
+        }
     }
 }
