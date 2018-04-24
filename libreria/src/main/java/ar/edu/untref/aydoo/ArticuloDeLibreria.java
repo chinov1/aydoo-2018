0b02ea2f6fc3d0a9d9b0de6284825ffd81777609
Maxi Biscardi <maxibiscar@gmail.com>
2018-04-24 00:00:51 -0300

    agregada doc

--- libreria/src/main/java/ar/edu/untref/aydoo/ArticuloDeLibreria.java
+++ libreria/src/main/java/ar/edu/untref/aydoo/ArticuloDeLibreria.java
@@ -3,7 +3,15 @@ package ar.edu.untref.aydoo;
 public class ArticuloDeLibreria extends Producto{
 
     public ArticuloDeLibreria(int precio){
-        super((int)(precio*1.21));
-
+        super(precio);
     }
+
+    public int getPrecio(){
+       return precioConIva();
+    }
+
+    public int precioConIva(){
+        return (int)(this.precio*1.21);
+    }
+
 }
