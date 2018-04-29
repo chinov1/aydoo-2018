06980c69c66de8967d4f101a6ebaa8247bc986ec
Maxi Biscardi <maxibiscar@gmail.com>
2018-04-29 00:38:10 -0300

    se modifico como se leen los comandos, y se modifico la forma de mostrar

--- fibo3/src/main/java/ar/edu/untref/aydoo/Program.java
+++ fibo3/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -11,36 +11,69 @@ public class Program {
 
     public static void main(final String[] arg) {
 
-        String textoDeSalida;
+        String textoDeSalida =  "";
+        char horizontalVertical = 'h';
+        char directaInversa = 'd';
+        char listaSumatoria = 'l';
         String comandoOrientaciones = arg[0];
         int numerosaMostrar = Integer.parseInt(arg[arg.length-1]);
+/*
         try {
             numerosaMostrar = Integer.parseInt(comandoOrientaciones);        //Busco una excepcion parseando letras
             comandoOrientaciones = "-o=hd";                                  //si pasa no son letras, y fue invocado de la manera "vieja", asumo horizontal y directa
         } catch (Exception e) {
             //numerosaMostrar = Integer.parseInt(arg[1]);
+        }*/
+
+        String principioDelComando = "";
+        for (String comandoActual:arg) {
+            if (comandoActual.length()>3) {
+                principioDelComando = comandoActual.substring(0, 3);
+                switch (principioDelComando) {
+                    case "-o=":
+                        horizontalVertical = comandoActual.charAt(3);
+                        directaInversa = comandoActual.charAt(4);
+                        break;
+                    case "-m=":
+                        listaSumatoria = comandoActual.charAt(3);
+                        break;
+                }
+
+            }
         }
 
-        if (comandoOrientaciones.startsWith("-o=") && (comandoOrientaciones.charAt(3) == 'h'  || comandoOrientaciones.charAt(3) == 'v') &&  (comandoOrientaciones.charAt(4) == 'i'  || comandoOrientaciones.charAt(4) == 'd')) {
-                Fibonacci fibo = new Fibonacci(numerosaMostrar);        //calculo serie
+        Fibonacci fibo = new Fibonacci(numerosaMostrar);
+        if (directaInversa == 'i') {                 //la invierto si hace falta
+            fibo.invertir();
+        }
 
-                if (comandoOrientaciones.charAt(4) == 'i') {                 //la invierto si hace falta
-                    fibo.invertir();
-                }
-
+        switch (listaSumatoria) {
+            case 'l':
+                if ((horizontalVertical == 'h' || horizontalVertical == 'v') && (directaInversa == 'i' || directaInversa == 'd')) {
                 textoDeSalida = "fibo<" + numerosaMostrar + ">:";     //muestro
-                switch (comandoOrientaciones.charAt(3)) {
-                    case 'h':   textoDeSalida += fibo.mostrarFiboHorizontal();
-                                break;
-                    case 'v':   textoDeSalida += fibo.mostrarFiboVertical();
-                                break;
+                switch (horizontalVertical) {
+                    case 'h':
+                        textoDeSalida += fibo.mostrarFiboHorizontal();
+                        break;
+                    case 'v':
+                        textoDeSalida += fibo.mostrarFiboVertical();
+                        break;
                 }
 
-        } else textoDeSalida = "Opciones no validas";
+                } else textoDeSalida = "Opciones no validas";
+                break;
+            case 's':
+                textoDeSalida = "fibo<" + numerosaMostrar + ">s:";
+                if (horizontalVertical == 'h') textoDeSalida += " ";
+                if (horizontalVertical == 'v') {textoDeSalida += "\r\n";}
+                textoDeSalida += fibo.sumatoria();
+                break;
+        }
 
         if(arg.length > 1) {
             if (arg[1].startsWith("-f=")) {
                 String archivoSalida = arg[1].substring(3);
+                System.out.println("fibo<" + numerosaMostrar + "> guardado en " + archivoSalida);
                 PrintWriter writer = null;
                 try {
                     writer = new PrintWriter(archivoSalida, "UTF-8");
